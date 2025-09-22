8f4bdb6b3fdb00dbca30039b5dccf834e58786d0
Javier C <javier.cuevas.asu@gmail.com>
2025-09-21 21:17:03 -0700

    Create grant-basics.sql

---
+++ grant-basics.sql
@@ -0,0 +1,4 @@
+GRANT USAGE ON DATABASE <database_name> TO ROLE <role_name>;
+GRANT USAGE ON SCHEMA <database_name>.<schema_name> TO ROLE <role_name>;
+GRANT SELECT ON ALL TABLES IN SCHEMA <database_name>.<schema_name> TO ROLE <role_name>;
+GRANT SELECT ON FUTURE TABLES IN SCHEMA <database_name>.<schema_name> TO ROLE <role_name>;
