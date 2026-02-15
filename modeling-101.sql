3f9ace6b729f7f18f341bc44b7a9f9f9481cd2b2
Javier C <javier.cuevas.asu@gmail.com>
2026-02-14 18:14:26 -0700

    Update modeling-101.sql

--- modeling-101.sql
+++ modeling-101.sql
@@ -18,3 +18,28 @@ CREATE TABLE ACCOUNT
     CONSTRAINT PK_2 PRIMARY KEY ( PERSON_ID, ACCOUNT_ID ),
     CONSTRAINT FK_1 FOREIGN KEY ( PERSON_ID ) REFERENCES PERSON (    PERSON_ID )
 );
+
+
+CREATE OR REPLACE TABLE employee
+(
+    employee_skey      integer NOT NULL AUTOINCREMENT START 1     INCREMENT 1,
+    employee_bkey      varchar(10) NOT NULL,
+    name               varchar NOT NULL,
+    social_security_id number(8,0) NOT NULL,
+    healthcare_id      integer NOT NULL,
+    birth_date         date NOT NULL,
+    CONSTRAINT pk_employee_skey PRIMARY KEY ( employee_skey ),
+    CONSTRAINT ak_employee_bkey UNIQUE ( employee_bkey ),
+    CONSTRAINT ak_healthcare_id UNIQUE ( healthcare_id ),
+    CONSTRAINT ak_ss_id         UNIQUE ( social_security_id )
+);
+CREATE OR REPLACE TABLE employee_of_the_month
+(
+    month         date NOT NULL,
+    employee_bkey varchar(10) NOT NULL,
+    awarded_for   varchar NOT NULL,
+    comments      varchar NOT NULL,
+    CONSTRAINT pk_employee_of_the_month_month PRIMARY KEY ( month ),
+    CONSTRAINT fk_ref_employee FOREIGN KEY ( employee_bkey )
+    REFERENCES employee ( employee_bkey )
+);
